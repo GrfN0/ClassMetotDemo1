866788858a6b592491756d1d8bbf18f74f38658a
samet yılmaz <samet_yilmaz07@hotmail.com>
2021-01-20 11:06:33 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteriler)
+        {
+            Console.WriteLine("Hoşgeldiniz!" + " " + musteriler.Ad + " " + musteriler.Soyad + " - " + musteriler.Id + " ");
+                
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("---------------- Müşteri Listesi -----------------");
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " - " + musteri.Id + " ");
+            Console.WriteLine("----------------- Liste Sonu --------------");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Müşteri silindi :" + " " + musteri.Ad + " " + musteri.Soyad + " - " + musteri.Id + " ");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 123456789;
+            musteri1.Ad = "Samet";
+            musteri1.Soyad = "Yılmaz";
+            musteri1.HesapNo = 789456123;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 321654987;
+            musteri2.Ad = "Mehmet";
+            musteri2.Soyad = "Korkmaz";
+            musteri2.HesapNo = 456123789;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Id : " + musteri.Id);
+                Console.WriteLine("Müşteri Ad : " + musteri.Ad);
+                Console.WriteLine("Müşteri Soyad : " + musteri.Soyad);
+                Console.WriteLine("Müşteri Hesap No : " + musteri.HesapNo);
+            }
+
+            Console.WriteLine("------Metotlar-------");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri2);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteri1);
+
+
+
+
+        }
+    }
+}
